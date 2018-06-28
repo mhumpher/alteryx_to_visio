33b3bd9cd4f02d8d4bfaa05524b6d630e93076d8
mhumpher <mhumpher@gmail.com>
2018-06-28 09:06:14 -0500

    Update AlteryxField.py
    Use xml objects to hold data and build functions around those to extract data.

--- AlteryxField.py
+++ AlteryxField.py
@@ -1,33 +1,24 @@
-#XML file does not alway provide all of information on fields, such as dataType and size
-#May need to develop a way to dynamically fill in this information when searching the workflow
-#This will generally not be possible
-
 import xml.sax.saxutils as saxutils
 import re
 
 class AlteryxField:
-    def __init__(self, name, size = "", dataType = "", selected = True, rename = "", formulaExp = ""):
-        self.name = name
-        self.dataType = dataType
-        self.size = size
+    def __init__(self, field_xml, parent):
+        self.__field_xml__ = field_xml
+        self.__parent__ = parent
+        self.name = self.__field_xml__.attrib['field']
+        self.outputName = self.__getOutputName__()
+        self.attributes = self.__field_xml__.attrib
+        #fieldDep will be a dictionary of the field names that this field depends upon.
+        #The item of each key will be a dictionary of the toolID (key) with a list of
+        #AlteryxField objects (item) that this field depends upon in that Tool. 
+        #{'field1':{5:[obj1, obj2]}, 'field2': {7:[obj3, obj4], 8:[obj5]}}
+        self.fieldDep = {}        
         
-        if rename == "":
-            self.rename = name
-        else:
-            self.rename = rename
-                
-        self.selected = selected
-        #Each dictionary key will contain a list of ALteryxTool objects that 
-        #this field is dependent upon. We need a list of Tools, because with a
-        #Union Tool a field may have multiple definitions and this field is
-        #dependent upon all of them
-        self.fieldDep = {}
-        self.formulaExp = self.normFormulaExp(formulaExp)
-        
-        if formulaExp != "":
-            self.parseExpFieldsDep()
+        if 'expression' in self.attributes:
+            self.formulaExp = self.__normFormulaExp__(self.attributes['expression'])
+            self.__parseExpFieldsDep__()
             
-    def parseExpFieldsDep(self):
+    def __parseExpFieldsDep__(self):
         #Need to remove comments
         #both "\\ ... \n" and "\* ... *\" comments
         #pick out fields from use of brackets
@@ -35,16 +26,29 @@ class AlteryxField:
         temp = re.sub('//.+?\\n', '', self.formulaExp) #remove comment lines
         temp = re.sub('/\*.+?\*/','', temp) #remove comment blocks
         fieldList = re.sub('\[.*?\]', '',temp)
-        for f in fieldList:
+        distinctFieldList = list(set(fieldList))
+        for f in distinctFieldList:
             f = f.replace('[', '')
             f = f.replace(']', '')
-            self.fieldDep[f] = []
-        pass
+            self.fieldDep[f] = {} 
+        #pass
     
-    def normFormulaExp(self, formulaExp):
+    def __normFormulaExp__(self, formulaExp):
         tempExp = saxutils.unescape(formulaExp)
         tempExp = tempExp.replace('&quote;', '"')
         tempExp = tempExp.replace('&#xA;', '\n')
-        
-        
         return tempExp
+        
+    def toolIdDepend(self, fieldname):
+        if bool(self.fieldDep) and fieldname in self.fieldDep:
+            return list(self.fieldDep[fieldname].keys())
+        else:
+            return []
+    
+    def __getOutputName__(self):
+        if 'rename' in self.__field_xml__.attrib:
+            return self.__field_xml__.attrib['rename']
+        elif 'field' in self.__field_xml__.attrib:
+            return self.__field_xml__.attrib['field']
+        else:
+            return ""
