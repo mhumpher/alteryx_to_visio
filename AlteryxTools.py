33d1cd65586aaae03a2580e10659636986b8e830
mhumpher <mhumpher@gmail.com>
2018-06-28 09:07:16 -0500

    Update AlteryxTools.py

--- AlteryxTools.py
+++ AlteryxTools.py
@@ -1,6 +1,8 @@
-class AlteryxTool:   
-    
-    def __init__(self, toolId, toolType, x, y):
+import AlteryxField as AF
+
+class AlteryxTool:
+   
+    def __init__(self, toolId, toolType, x, y, tool_xml):
         self.toolId = toolId
         self.toolType = toolType
         self.x = x
@@ -8,6 +10,18 @@ class AlteryxTool:
         self.consIn = {}
         self.consOut = {}
         self.fields = {}
+        self.__tool_xml__ = tool_xml
+        
+        self.__populateFields__()
+
+    def __getFieldElements__(self):
+        return self.__getNodesWithAttrib__(attrib = 'field')
+        
+    def __getNodesWithAttrib__(self, attrib):
+        return self.__tool_xml__.findall('.//*[@' + attrib + ']')
+        
+    def getFieldNames(self):
+        return self.fields.keys()
         
     def addConIn(self, toolId, altTool):
         self.consIn[toolId] = altTool
@@ -23,40 +37,26 @@ class AlteryxTool:
     #If no connections in, tool is considered initial
     def isInitial(self):
         return not bool(self.consIn)
+    
+    def __populateFields__(self):
+        fieldNodes = self.__getFieldElements__()
+        for fN in fieldNodes:
+            altField = AF.AlteryxField(fN, self)
+            self.fields[fN.attrib['field']] = altField
+            
+    def checkOutputFieldNames(self, fieldname):
+        #if there are no renames and the fieldname is in the dictionary, then true
+        for key in self.fields:
+            if self.fields[key].outputName == fieldname:
+                return False
+        return False
+        
+    def getFieldFromOutputName(self,fieldname):
+        for key in self.fields:
+            if self.fields[key].outputName == fieldname:
+                return self.fields[key]
+        return ""
+                        
 
-###############################################################################
-#
-#Special Tools
-#- Documentation
-#
-###############################################################################        
-class DocumentationTool:
-    def __init__(self, toolId, toolType, x, y, width, height):
-        self.toolId = toolId
-        self.toolType = toolType
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        
-class ToolContainer(DocumentationTool):
-    def __init__(self, toolId, toolType, x, y, width, height, caption):
-        self.toolId = toolId
-        self.toolType = toolType
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.caption = caption
-        self.childTools = {}
-        
-class TextBoxTool(DocumentationTool):
-    def __init__(self, toolId, toolType, x, y, width, height, text):
-        self.toolId = toolId
-        self.toolType = toolType
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
+            
         
